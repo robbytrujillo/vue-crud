27be42adaefa46010b6a170ccad0b7d86f26b094
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-27 06:23:01 +0700

    Introduction vue3-crud

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,32 @@
-<script setup>
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div class="container">
+        <router-link :to="{}" class="navbar-brand">HOME</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupporteContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link :to="{ name: 'posts.index'}" class="nav-link active" aria-current="page">POSTS</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0" role="search">
+            <a href="https://robbyilhamk.netlify.app/" target="_blank" class="btn btn-success">robbyilhamk.netlify.app</a>
+          </ul>
+        </div>
+      </div>
+
+    </nav>
+
+    <!-- render router view -->
+     <router-view></router-view>
+     
+  </div>
+</template>
+
+<!-- <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -27,4 +55,4 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+</style> -->
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// 1 import create from Vue
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+// 2. import component App
+import App from './App.vue';
+
+// import config router
+import router from './router';
+
+// create App Vue
+const app = createApp(App);
+
+// gunakan "router" di Vue dengan plugin "use"
+app.use(router);
+
+app.mount('#app');
+
+// import './style.css'
+// createApp(App).mount('#app')
