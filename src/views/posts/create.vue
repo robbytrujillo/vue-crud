97ed6b8d09993795cfcbab0a4a3bd1b4615e39d6
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-27 13:52:46 +0700

    Introduction vue3-crud | edit page

--- src/views/posts/create.vue
+++ src/views/posts/create.vue
@@ -6,7 +6,7 @@
     import { useRouter } from "vue-router";
 
     // import api
-    import Api from "../../api";
+    import api from "../../api";
 
     // init router
     const router = useRouter();
@@ -37,8 +37,8 @@
         // store data with API
         await api.post('/api/posts', formData)
             .then(() => {
-            // redirect 
-            router.push({ name: "/posts" });
+                // redirect 
+                router.push({ path: "/posts" });
             })
 
             .catch((error) => {
@@ -49,7 +49,7 @@
 </script>
 
 <template>
-    <div class="container mt-5 mb-5">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div class="card border-0 rounded shadow">
@@ -62,6 +62,21 @@
                                     <span>{{ errors.image[0] }}</span>
                                 </div>
                             </div>
+                            <div class="mb-3">
+                                <label class="form-label fw-bold">Title</label>
+                                <input type="text" class="form-control" v-model="title" placeholder="Title Post">
+                                <div v-if="errors.title" class="alert alert-danger mt-2">
+                                    <span>{{ errors.title[0] }}</span>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label fw-bold">Content</label>
+                                <textarea class="form-control" v-model="content" rows="5" placeholder="Content Post"></textarea>
+                                <div v-if="errors.content" class="alert alert-danger mt-2">
+                                    <span>{{ errors.content[0] }}</span>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-md btn-primary rounded-sm shadow border-0">Save</button>
                         </form>
                         <!-- HALAMAN POST CREATE -->
                     </div>
